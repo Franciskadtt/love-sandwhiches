5d38886037a99387c9e4687cefab1d4f130e4a88
Franciskadtt <franciskadtt@gmail.com>
2022-04-11 14:45:56 +0000

    Adjust validation to check that all values provided are integers

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
+
     sales_data = data_str.split(",")
     validate_data(sales_data)
 
@@ -36,6 +37,8 @@ def validate_data(values):
     # Try & Run code with no errors if the data is valid
     # Lets you test a block of code for errors.
     try:
+        # Convert each value in values list into an integer
+        [int(value) for value in values]
         # Check how many values we have in our values list
         # if length of values is not 6
         if len(values) != 6:
